a52ca28891623e27ecc54a3a343097b6c8e3e24c
yeard-morshed <yeardmorshed1999@gmail.com>
2022-12-21 00:50:31 +0600

    coustom_Scroll

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/customScrollbarWithSingleChildScrollView.dart';
+import '../widgets/mainevent_card.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,14 +13,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var dwidth = MediaQuery.of(context).size.width;
+    var dheight = MediaQuery.of(context).size.height;
+    //final List<String> entries = <String>[    'A',      'B',      'C',    ];
+    ScrollController con = ScrollController();
+
     return Scaffold(
-      body: Container(
-        color: Colors.black38,
-        child: Text(
-          'Home Page',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 40,
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.black38,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 5000,
+                height: dheight - (dheight - 102),
+                color: Color.fromARGB(255, 204, 243, 154),
+                child: CustomScrollbarWithSingleChildScrollView(
+                    controller: con,
+                    scrollDirection: Axis.horizontal,
+                    child: Container(
+                      height: 3000,
+                      child: Text(
+                          'ttrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr66666666666666666666666666666666666666666666666666666666666677777777777777777777777777777777777777777777777777777777777799999999999999999999999999999999999999999999999999999999999999999999990000000000000000000000000000000000000000000'),
+                    )),
+              ),
+              //
+
+              Container(
+                width: dwidth,
+                height: dheight - 102,
+                color: Colors.red,
+                child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Container(
+                      height: 3000,
+                      child: Text('tt'),
+                    )),
+              )
+            ],
           ),
         ),
       ),
---
+++ lib/widgets/mainevent_card.dart
@@ -0,0 +1,16 @@
+import 'package:getwidget/getwidget.dart';
+import 'package:flutter/material.dart';
+
+class MainEventCard extends StatelessWidget {
+  const MainEventCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      width: 200,
+      color: Colors.blue,
+      child: Text('hee'),
+    );
+  }
+}
